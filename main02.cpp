fae79f3d5aa298c89ec86497a5a3ca352e2f2397
dongin1002 <155039243+dongin1002@users.noreply.github.com>
2026-07-15 11:00:03 +0900

    Create main02.cpp

---
+++ main02.cpp
@@ -0,0 +1,29 @@
+#include "timePtr.h"
+#include <memory>
+
+int main()
+{
+    using namespace KwakMinChae2449102;
+    timePtr t1{13,30}; t1.print();
+    timePtr t2{t1}; t2.print();
+    timePtr t3{std::move(t2)}; t2.print(); t3.print();
+    t2 = std::move(t3);
+    t3 = t2;
+
+    std::unique_ptr<timePtr> up1;
+    auto up2 = std::make_unique<timePtr>(14, 40);
+    up1 = std::move(up2); up1->print();
+
+    auto sp1 = std::make_shared<timePtr>(15, 50);
+    std::shared_ptr<timePtr> sp2{sp1};
+    std::cout << sp1.use_count() << " " << sp2.use_count() << "\n";
+
+    std::weak_ptr<timePtr> wp1{sp1};
+    std::cout << sp1.use_count() << " " << sp2.use_count() << "\n";
+
+    if(!wp1.expired())
+        sp1 = wp1.lock();
+    std::cout << sp1.use_count() << " " << sp2.use_count() << "\n";
+
+    return 0;
+}
